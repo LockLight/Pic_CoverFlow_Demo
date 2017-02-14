3d9d9f1af6d5097757b6aac0860a53e8c7359f7c
LockLight <Locklightz@gmail.com>
2017-02-14 20:08:34 +0800

    Initial Commit

---
+++ 图片coverFlow特效/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  图片coverFlow特效
+//
+//  Created by locklight on 17/2/14.
+//  Copyright © 2017年 LockLight. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 图片coverFlow特效/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  图片coverFlow特效
+//
+//  Created by locklight on 17/2/14.
+//  Copyright © 2017年 LockLight. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
